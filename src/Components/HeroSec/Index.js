ca194e85fd976fb4be8f55690d3a2fb6f092950a
Jarmo van de Seijp <dewebbutler@gmail.com>
2022-03-16 22:14:03 +0100

    initial commit

--- src/Components/HeroSec/Index.js
+++ src/Components/HeroSec/Index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Index.css'
 import poweredimg from '../../Assets/poweredimg.png'
+import Moralis from 'moralis/types';
+import { useMoralis } from 'react-moralis';
 
 
 function HeroSec () {
+
+    const{
+        account,
+        isAuthenticated,
+        user,
+        Moralis,         
+    }= useMoralis()
+
     return(
         <div className="herosecparent">
         <div className="heroseccon">
@@ -12,8 +22,8 @@ function HeroSec () {
             </div>
             <div className='herosecbtn'>
                 <button>SHADOWBOX</button>
-                <button>TRADE ON UNIWSWAP  </button>
-                <p>TERMS APPLY</p>
+                <a href="https://www.uniswap.com, _blank"><button >TRADE ON UNIWSWAP </button> </a>
+                <a href="/terms"><p>TERMS APPLY</p></a>
             </div>
         </div>
         <div className='poweredimg'>
